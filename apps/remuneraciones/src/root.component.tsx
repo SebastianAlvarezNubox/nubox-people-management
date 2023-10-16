505c423a692bf689de5fc7acf34ab65799719126
SebastianAlvarezNubox <sebastian.alvarez@nubox.com>
2023-10-16 16:31:25 -0300

    se agrega primera vertical completa

--- apps/remuneraciones/src/root.component.tsx
+++ apps/remuneraciones/src/root.component.tsx
@@ -1,3 +1,78 @@
+import { useEffect, useReducer, useState } from "react";
+
+interface Departamento {
+  codigo: string;
+  descripcion: string;
+}
+
+
+function getCookies() {
+  var cookies = {};
+  var theCookies = document.cookie.split(';');
+  var aString = '';
+  for (var i = 1 ; i <= theCookies.length; i++) {
+      let singleCookie =  theCookies[i-1].split('=');
+    cookies[singleCookie[0].trim()] = singleCookie[1];
+  }
+  return cookies;
+}
+
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const [departamentos, setDepartamentos] = useState<Departamento[]>([]);
+
+  useEffect(() => {
+
+    const cookies = getCookies();
+
+    //COOKIE%5FNORMAL%5FSERVIPYME
+    //WS-02951AF7
+
+    let sessionId = cookies['COOKIE%5FNORMAL%5FSERVIPYME'].replace('WS-','')
+		sessionId = parseInt(sessionId, 16)
+
+    fetch("http://localhost:8081/remugrlbff/api/departamentos/?sujetoContableId=186831", {
+      method: "GET", // or 'PUT'
+      headers: {
+        "x-session-web-id": sessionId,
+      },
+    }).then(async (response) => {
+      const text = await response.text()
+      console.log(text)
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(text,"text/xml");
+      const departamentosXML = xmlDoc.getElementsByTagName('departamento');
+      
+      const departamentos:Departamento[] = [];
+      for(let x = 0; x < departamentosXML.length; x++){
+        departamentos.push({
+          codigo: departamentosXML[x].getElementsByTagName('codigo')[0].childNodes[0].nodeValue,
+          descripcion: departamentosXML[x].getElementsByTagName('descripcion')[0].childNodes[0].nodeValue
+        })
+      }
+      setDepartamentos(departamentos)
+    })
+  }, [setDepartamentos])
+  
+  return <>
+    <section>{props.name} is mounted!</section>
+    <table>
+      <thead>
+        <tr>
+          <th>codigo</th>
+          <th>descripcion</th>
+        </tr>
+      </thead>
+      <tbody>
+          {
+           departamentos.length > 0 && departamentos.map((departamento) => {
+              return <tr key={departamento.codigo}>
+                <td>{departamento.codigo}</td>
+                <td>{departamento.descripcion}</td>
+              </tr>
+            })
+          }
+      </tbody>
+    </table>
+  </>;
 }
