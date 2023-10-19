e81df0fdac1f65092cc90e6cb66e6650f054267f
Sebastian <sebastian.alvarez@nubox.com>
2023-10-19 17:05:19 -0300

    add tailwind directly to mfe

--- apps/remuneraciones/dist/root.component.d.ts
+++ apps/remuneraciones/dist/root.component.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="react" />
+import "./global.css?modules=false";
 export default function Root(props: any): JSX.Element;
--- apps/remuneraciones/src/nubox-remuneraciones.tsx
+++ apps/remuneraciones/src/nubox-remuneraciones.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
